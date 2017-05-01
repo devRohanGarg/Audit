4854b31f1ed4a4159d838eb6eec1f8e18709223f
Rohan Garg <dev.RohanGarg@gmail.com>
2017-05-01 15:30:50 +0530

    refactor: Rename package

--- app/src/main/java/com/audit/LoginActivity.java
+++ app/src/main/java/com/audit/LoginActivity.java
@@ -1,13 +1,19 @@
-package audit.com.audit;
+package com.audit;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
