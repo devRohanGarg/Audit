10e5837c6f6221e651f6d5d75885218e91ca138d
Rohan Garg <dev.RohanGarg@gmail.com>
2017-05-03 18:35:26 +0530

    feat: Add MainActivity and change flow of control to MainActivity

--- app/src/main/java/com/audit/MainActivity.java
+++ app/src/main/java/com/audit/MainActivity.java
@@ -1,50 +1,213 @@
 package com.audit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.TwitterAuthProvider;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    AppCompatButton mLoginButton;
+    TextView mNameTextView;
+    TextView mEmailTextView;
+    private String TAG = "MainActivity";
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private TwitterAuthClient mTwitterAuthClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Configure Twitter SDK
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(
+                getString(R.string.twitter_consumer_key),
+                getString(R.string.twitter_consumer_secret));
+        Fabric.with(this, new Twitter(authConfig));
+
+        // Inflate layout (must be done after Twitter is configured)
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
+
+        Toolbar toolbar = ButterKnife.findById(this, R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = ButterKnife.findById(this, R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = ButterKnife.findById(this, R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        mLoginButton = ButterKnife.findById(navigationView.getHeaderView(0), R.id.login_button);
+        mNameTextView = ButterKnife.findById(navigationView.getHeaderView(0), R.id.name);
+        mEmailTextView = ButterKnife.findById(navigationView.getHeaderView(0), R.id.email);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }// [END on_start_check_user]
+
+    // [START on_activity_result]
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result to the Twitter login button.
+        mTwitterAuthClient.onActivityResult(requestCode, resultCode, data);
+    }
+    // [END on_activity_result]
+
+    // [START auth_with_twitter]
+    private void handleTwitterSession(TwitterSession session) {
+        Log.d(TAG, "handleTwitterSession:" + session);
+        // [START_EXCLUDE silent]
+        showProgressDialog();
+        // [END_EXCLUDE]
+
+        AuthCredential credential = TwitterAuthProvider.getCredential(
+                session.getAuthToken().token,
+                session.getAuthToken().secret);
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    // [END auth_with_twitter]
+
+    void login() {
+        // [START initialize_twitter_login]
+        mTwitterAuthClient = new TwitterAuthClient();
+        mTwitterAuthClient.authorize(MainActivity.this, new Callback<TwitterSession>() {
+            @Override
+            public void success(final Result<TwitterSession> result) {
+                Log.d(TAG, "twitterLogin:success" + result);
+                handleTwitterSession(result.data);
+            }
+
+            @Override
+            public void failure(final TwitterException e) {
+                Log.w(TAG, "twitterLogin:failure", e);
+                updateUI(null);
+            }
+        });
+        // [END initialize_twitter_login]
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        Twitter.logOut();
+
+        updateUI(null);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+        NavigationView navigationView = ButterKnife.findById(this, R.id.nav_view);
+        Menu menu = navigationView.getMenu();
+        if (user != null) {
+            Log.d(TAG, user.getDisplayName());
+            Log.d(TAG, user.getEmail());
+            Log.d(TAG, String.valueOf(user.getPhotoUrl()));
+
+            mLoginButton.setVisibility(View.GONE);
+
+            menu.findItem(R.id.nav_logout).setVisible(true);
+
+            mNameTextView.setText(user.getDisplayName());
+            mEmailTextView.setText(user.getEmail());
+
+            mNameTextView.setVisibility(View.VISIBLE);
+            mEmailTextView.setVisibility(View.VISIBLE);
+        } else {
+            mNameTextView.setVisibility(View.GONE);
+            mEmailTextView.setVisibility(View.GONE);
+
+            mLoginButton.setVisibility(View.VISIBLE);
+            menu.findItem(R.id.nav_logout).setVisible(false);
+        }
+    }
+
+    @OnClick(R.id.fab)
+    public void onClick(View view) {
+        Snackbar.make(view, "Made with Love", Snackbar.LENGTH_LONG).setAction("", null).show();
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = ButterKnife.findById(this, R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -80,13 +243,13 @@ public class MainActivity extends BaseActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_feedback) {
+        if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_logout) {
+            signOut();
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = ButterKnife.findById(this, R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
